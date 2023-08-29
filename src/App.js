f2394498a473baec1b893db4b08e7cfb191cbe3b
Kritika Rawat <kritika3n@gmail.com>
2023-08-29 16:11:53 +0530

    First Step

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './screens/Login';
+
+function App() {
+  return (
+    <Router>
+    <div> 
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/Login" element={<Login/>} />
+      </Routes>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+
+export default function Home() {
+  return (
+    <div>
+      <div><Navbar/></div>
+      <div>body</div>
+      <div><Footer/></div>
+    </div>
+  )
+}
